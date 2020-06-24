c9989c980ff02e7cc3eb03bb183bd3f34d17ce31
NataliaVolik <log.litana@gmail.com>
2020-06-24 15:50:15 +0600

    validators input

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-contact',
@@ -8,13 +8,30 @@ import { FormGroup } from "@angular/forms";
 })
 export class ContactComponent implements OnInit {
   form: FormGroup
+
   constructor() { }
 
   ngOnInit(): void {
-    this.form = new FormGroup({})
+    this.form = new FormGroup({
+      name: new FormControl( '', [
+        Validators.required,
+        Validators.maxLength(25),
+        Validators.minLength(2)
+      ]),
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required
+      ]),
+      text: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(256)
+      ])
+    })
   }
   submit(){
-    console.log('Form submitted: ', this.form);
+    console.log('Form: ', this.form);
+    const formData = {...this.form.value}
+    console.log('Form Data: ', formData);
   }
 
 }
